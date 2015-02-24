1340e3247a42300c440358a33cfb1e03b88d4426
Abdullah Öğük <aoguk1102@gmail.com>
2015-02-24 10:11:35 +0200

    -

---
+++ py6.py
@@ -0,0 +1,10 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+
+import string
+metin = "tbyksr çsn jücho elu gloglu"
+kaynak= "defghijklmnoprstuvyzabc"
+hedef = "abcdefghijklmnoprstuvyz"
+cevir = string.maketrans(kaynak,hedef)
+sonuc = metin.translate(cevir)
+print soncevir
